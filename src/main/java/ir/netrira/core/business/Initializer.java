7f1a2840b15c0d9f2a7a7e68b5b3ec1393e70c74
dnaiel zangene <46925439+danielzangene@users.noreply.github.com>
2023-02-16 17:04:43 +0330

    init admin users impl systemaccess

--- src/main/java/ir/netrira/core/business/Initializer.java
+++ src/main/java/ir/netrira/core/business/Initializer.java
@@ -3,10 +3,12 @@ package ir.netrira.core.business;
 import ir.netrira.core.application.filter.access.AccessFilter;
 import ir.netrira.core.application.filter.access.GroupSystemAccessRepository;
 import ir.netrira.core.application.filter.access.SystemAccessRepository;
+import ir.netrira.core.application.filter.auth.repository.UserRepository;
 import ir.netrira.core.business.personnel.PersonnelCode;
 import ir.netrira.core.business.utils.calendar.CalendarRepo;
 import ir.netrira.core.business.utils.calendar.DateUtil;
 import ir.netrira.core.business.utils.element.ElementRepo;
+import ir.netrira.core.models.application.personnel.User;
 import ir.netrira.core.models.application.systemaccess.GroupSystemAccess;
 import ir.netrira.core.models.application.systemaccess.SystemAccess;
 import ir.netrira.core.models.application.utils.Calendar;
@@ -14,6 +16,7 @@ import ir.netrira.core.models.application.utils.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -26,6 +29,12 @@ public class Initializer {
 
     @Autowired
     ElementRepo elementRepo;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     SystemAccessRepository systemAccessRepository;
@@ -44,10 +53,11 @@ public class Initializer {
     @PostConstruct
     private void init() {
 
-//        initElement();
-//        initSystemAccess();
-//        initGroupSystemAccess();
-//        initGroupSystemAccessAssign();
+        initElement();
+        initSystemAccess();
+        initGroupSystemAccess();
+        initGroupSystemAccessAssign();
+        initUsers();
 //        initCalendar();
     }
 
@@ -117,6 +127,7 @@ public class Initializer {
     private void addGroupSystemAccess() {
         groupSystemAccesses.add(new GroupSystemAccess("ماکرو سیستم", PersonnelCode.MACRO_USER, Arrays.asList()));
         groupSystemAccesses.add(new GroupSystemAccess("مدیر سیستم", PersonnelCode.SUPER_ADMIN_USER, Arrays.asList()));
+        groupSystemAccesses.add(new GroupSystemAccess("ناشناس", PersonnelCode.ANONYMOUS_USER, Arrays.asList()));
         groupSystemAccesses.add(new GroupSystemAccess("مدیر ارشد", PersonnelCode.ADMIN_USER, Arrays.asList()));
         groupSystemAccesses.add(new GroupSystemAccess("مدیر شعبه", PersonnelCode.MANAGER_USER, Arrays.asList()));
         groupSystemAccesses.add(new GroupSystemAccess("تحلیلگر", PersonnelCode.ANALYST_USER, Arrays.asList()));
@@ -127,59 +138,86 @@ public class Initializer {
         systemAccesses.add(new SystemAccess("POST", "/api/test", "test"));
     }
 
-    private void initElement() {
-        addElement();
-        for (Element e : elements) {
+    private void initUsers() {
+        List<User> users = Arrays.asList(
+                new User(
+                        "superuser",
+                        "",
+                        encoder.encode("1234567890"),
+                        "مدیر سیستم",
+                        elementRepo.findByCode(PersonnelCode.SUPER_ADMIN_USER).get()
+                ),
+                new User(
+                        "macro",
+                        "",
+                        encoder.encode("1234567890"),
+                        "ماکرو سیستم",
+                        elementRepo.findByCode(PersonnelCode.MACRO_USER).get()
+                )
+        );
+        for (User user : users) {
             try {
-                Optional<Element> elementOptional = elementRepo.findByCode(e.getCode());
-                if (!elementOptional.isPresent()) {
-                    Element element = new Element();
-                    element.setRootCode(e.getRootCode());
-                    element.setName(e.getName());
-                    element.setCode(e.getCode());
-                    elementRepo.save(element);
-                    logger.info("ELEMENT PERSISTS: " + element.getCode());
+                userRepository.save(user);
+                logger.info("ELEMENT PERSISTS: " + user.getUsername());
+            } catch (Exception exception) {
+                logger.error("ELEMENT NOT PERSISTS: " + user.getUsername());
+            }
+        }
+    }
+
+        private void initElement () {
+            addElement();
+            for (Element e : elements) {
+                try {
+                    Optional<Element> elementOptional = elementRepo.findByCode(e.getCode());
+                    if (!elementOptional.isPresent()) {
+                        Element element = new Element();
+                        element.setRootCode(e.getRootCode());
+                        element.setName(e.getName());
+                        element.setCode(e.getCode());
+                        elementRepo.save(element);
+                        logger.info("ELEMENT PERSISTS: " + element.getCode());
+                    }
+                } catch (Exception exception) {
+                    logger.error("ELEMENT NOT PERSISTS: " + e.getCode());
                 }
-            } catch (Exception exception) {
-                logger.error("ELEMENT NOT PERSISTS: " + e.getCode());
             }
+            logger.info("ALL ELEMENTS PERSIST.");
         }
-        logger.error("ALL ELEMENTS PERSIST.");
-    }
 
-    private void initSystemAccess() {
-        addSystemAccess();
-        for (SystemAccess access : systemAccesses) {
-            try {
-                systemAccessRepository.save(access);
-                logger.info("SystemAccess PERSISTS: " + access.getMethod() + "/" + access.getRequestUri());
-            } catch (Exception exception) {
-                logger.error("SystemAccess NOT PERSISTS: " + access.getMethod() + "/" + access.getRequestUri());
+        private void initSystemAccess () {
+            addSystemAccess();
+            for (SystemAccess access : systemAccesses) {
+                try {
+                    systemAccessRepository.save(access);
+                    logger.info("SystemAccess PERSISTS: " + access.getMethod() + "-" + access.getRequestUri());
+                } catch (Exception exception) {
+                    logger.error("SystemAccess NOT PERSISTS: " + access.getMethod() + "-" + access.getRequestUri());
+                }
             }
+            logger.info("ALL SystemAccess PERSIST.");
         }
-        logger.error("ALL SystemAccess PERSIST.");
-    }
-    private void initGroupSystemAccess() {
-        addGroupSystemAccess();
-        for (GroupSystemAccess groupAccess : groupSystemAccesses) {
-            try {
-                groupSystemAccessRepository.save(groupAccess);
-                logger.info("GroupSystemAccess PERSISTS: " + groupAccess.getName());
-            } catch (Exception exception) {
-                logger.error("GroupSystemAccess NOT PERSISTS: " + groupAccess.getName());
+        private void initGroupSystemAccess () {
+            addGroupSystemAccess();
+            for (GroupSystemAccess groupAccess : groupSystemAccesses) {
+                try {
+                    groupSystemAccessRepository.save(groupAccess);
+                    logger.info("GroupSystemAccess PERSISTS: " + groupAccess.getName());
+                } catch (Exception exception) {
+                    logger.error("GroupSystemAccess NOT PERSISTS: " + groupAccess.getName());
+                }
             }
+            logger.info("ALL GroupSystemAccess PERSIST.");
         }
-        logger.error("ALL GroupSystemAccess PERSIST.");
+
+        private void initGroupSystemAccessAssign () {
+
+        }
+
+        public static interface StaticValues {
+            Map<Integer, List<String>> offOfficialDays = new HashMap<>() {{
+                put(1401, Arrays.asList("1401/01/01", "1401/01/02", "1401/01/03", "1401/01/04", "1401/01/13", "1401/02/03", "1401/02/13", "1401/02/14", "1401/03/14", "1401/03/15", "1401/04/19", "1401/04/27", "1401/05/16", "1401/05/17", "1401/06/26", "1401/07/03", "1401/07/05", "1401/07/13", "1401/10/06", "1401/11/15", "1401/11/22", "1401/11/29", "1401/12/17", "1401/12/29"));
+            }};
+
+        }
     }
-
-    private void initGroupSystemAccessAssign() {
-
-    }
-
-    public static interface StaticValues {
-        Map<Integer, List<String>> offOfficialDays = new HashMap<>() {{
-            put(1401, Arrays.asList("1401/01/01", "1401/01/02", "1401/01/03", "1401/01/04", "1401/01/13", "1401/02/03", "1401/02/13", "1401/02/14", "1401/03/14", "1401/03/15", "1401/04/19", "1401/04/27", "1401/05/16", "1401/05/17", "1401/06/26", "1401/07/03", "1401/07/05", "1401/07/13", "1401/10/06", "1401/11/15", "1401/11/22", "1401/11/29", "1401/12/17", "1401/12/29"));
-        }};
-
-    }
-}
